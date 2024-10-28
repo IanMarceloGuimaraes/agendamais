d344311ef2ba93d77df8ecebb272f3d334935ad2
AEsir364 <alanclimaco07@gmail.com>
2024-10-28 15:00:32 -0400

    Esse formas com gradiente fica dando problema na hora de inicializar a página, aí ela carrega torta.

--- App/Views/home.php
+++ App/Views/home.php
@@ -1,16 +1,19 @@
 <?php include 'includes/header.php'; ?>
 
 <div class="container">
-    <div class="content">
-        <h1>Com o agenda +, você pode transformar o seu tempo em arte.</h1>
-        <a href="#" class="button">Entre</a>
-        <p>Ou venha fazer parte:</p>
-        <form action="#" method="post" class="form">
-            <input type="text" name="usuario" placeholder="Nome de Usuário *" required>
-            <input type="email" name="email" placeholder="Email *" required>
-            <input type="password" name="senha" placeholder="Crie uma senha *" required>
-            <button type="submit" class="button">Criar conta</button>
-        </form>
+    <div class="content" style="display: flex; align-items: flex-start;">
+        <div class="text-content" style="flex: 1;">
+            <h1>Com o agenda +,<br>você pode<br>transformar o seu<br>tempo em arte.</h1>
+            <a href="#"><button class="button">Entrar</button></a>
+            <p>Ou venha fazer parte :</p>
+            <form action="#" method="post" class="form">
+                <input type="text" name="usuario" placeholder="Nome de Usuário *" required>
+                <input type="email" name="email" placeholder="Email *" required>
+                <input type="password" name="senha" placeholder="Crie uma senha *" required>
+                <button type="submit" class="button">Criar conta</button>
+            </form>
+        </div>
+        <img src="Images/Home.png" alt="Descrição da imagem"/>
     </div>
 </div>
 
